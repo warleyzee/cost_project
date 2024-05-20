e1e165c277836646045f88eaa1cbf6d591c8696d
warleyzee <warleyzee@hotmail.com>
2024-05-20 12:30:39 +0100

    create page home

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -8,14 +8,14 @@ import logo from '../../../img/costs_logo.png';
 
 function Navbar(){
   return(
-    <nav class={styles.navbar}>
+    <nav className={styles.navbar}>
       <Container>
       <Link to='/'> <img src={logo} alt="Costs"/></Link>
-        <ul class={styles.list}>
-          <li class={styles.item}><Link to='/'>Home</Link></li>
-          <li class={styles.item}><Link to='/project'>Projects</Link></li>
-          <li class={styles.item}><Link to='/company'>Company</Link></li>
-          <li class={styles.item}><Link to='/contact'>Contact</Link></li>
+        <ul className={styles.list}>
+          <li className={styles.item}><Link to='/'>Home</Link></li>
+          <li className={styles.item}><Link to='/project'>Projects</Link></li>
+          <li className={styles.item}><Link to='/company'>Company</Link></li>
+          <li className={styles.item}><Link to='/contact'>Contact</Link></li>
         </ul>
         {/* <Link to='/newProject'>New Project</Link> */}
       </Container>
--- src/components/pages/home/HomeIndex.js
+++ src/components/pages/home/HomeIndex.js
@@ -1,6 +1,16 @@
+import styles from './Home.module.css';
+import LinkButton from '../../layout/linkButton/LinkButton';
+
+import savings from '../../../img/savings.svg';
+
 function Home(){
   return (
-    <h1>Page Home</h1>
+    <section className={styles.home_container}>
+      <h1>Wellcome to <span>COST</span></h1>
+      <p>Start manger your projects now</p>
+      <LinkButton to="/newProject" text="New Project" />
+      <img src={savings} alt="costs" />
+    </section>
   );
 }
 
