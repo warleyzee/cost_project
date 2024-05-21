b9b55f712b1cae0a58229bd416053000428b9d10
warleyzee <warleyzee@hotmail.com>
2024-05-21 15:57:51 +0100

    create button page project

--- src/components/pages/project/Project.js
+++ src/components/pages/project/Project.js
@@ -1,6 +1,9 @@
 import { useLocation } from 'react-router-dom'
 
 import Message from '../../layout/message/Message';
+import styles from './Project.module.css';
+import Container from '../../layout/container/container';
+import LinkButton from '../../layout/linkButton/LinkButton';
 
 function Project(){
 
@@ -11,9 +14,16 @@ function Project(){
   }
 
   return (
-    <div>
-      <h1>My Projects</h1>
-      {message && <Message type="success" msg={message} />}
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>My Projects</h1>
+        <LinkButton to="/newProject" text="Create Project" />
+      </div>
+        {message && <Message type="success" msg={message} />}
+        <Container custoClasse="start">
+          <p>Project</p>
+
+        </Container>
     </div>
   );
 }
