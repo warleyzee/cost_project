a04dd7279d48c1162d02ba3046df37fc0bea9bb7
warleyzee <warleyzee@hotmail.com>
2024-05-21 07:35:13 +0100

    conected to Json server API

--- src/components/form/select/Select.js
+++ src/components/form/select/Select.js
@@ -6,6 +6,9 @@ function Select({ text, name, option, handleOnChancge, value}){
       <label htmlFor={name}>{text}</label>
       <select name={name} id={name}>
         <option>Select</option>
+        {option.map((option) => (
+          <option value={option.id} key={option.id}> {option.name} </option>
+        ))}
       </select>
     </div>
   )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,32 @@
+import { useState, useEffect } from 'react';
+
 import style from './ProjectForm.module.css';
 import Input from '../form/input/Input';
 import Select from '../form/select/Select';
 import Button from '../form/button/Button';
 
 function ProjectForm({ btnText }){
+  const [categories, setCategories] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/categories",{
+    method: "GET",
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((resp) => resp.json())
+    .then((data) =>{
+      setCategories(data)
+    })  
+    .catch((err) => console.log(err ))
+  }, [])
+
   return(
     <form className={style.form} >
       <Input type="text" text="Project Name" name="name" placeholder="Insert Project Name" />
       <Input type="number" text="Project Price" name="price" placeholder="Insert Project Price" />
-      <Select name="category_id" text="Select Category" />
+      <Select name="category_id" text="Select Category" option={categories} />
       <Button text={btnText}/>
     </form>
   )
